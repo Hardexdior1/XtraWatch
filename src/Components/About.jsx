613cba4a87484fe3dc9ca3ca6320b0ee6abd4195
adeniran <adeniranquwam001@gmail.com>
2024-05-29 17:10:13 +0100

    added-count

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/ABout.css";
-import watch from '../images/watch-about.jpg'
-import watch2 from '../images/about-watch-3.jpg'
+import watch from "../images/watch-about.jpg";
+import watch2 from "../images/about-watch-3.jpg";
 import testimonial1 from "../images/testimonial-3.jpg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-import testimonial2 from '../images/testimonial-4.jpg'
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
+import testimonial2 from "../images/testimonial-4.jpg";
 
 const About = () => {
   const settings = {
@@ -17,250 +18,275 @@ const About = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const [counterOn, setCounterOn] = useState(false);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, 300);
   return (
     <div>
       <section className="aboutWrap">
-       <div className="aboutWrapSub">
-       <h1>
-          Get to Know us <span>better</span>
-        </h1>
+        <div className="aboutWrapSub">
+          <h1>
+            Get to Know us <span>better</span>
+          </h1>
 
-        <p>
-          With over 27 years of experience, we have been providing exceptional
-          watches services for commercial and residential properties throughout
-          Bellingham. If you're looking for any type of plumbing repairs,
-          installations or replacements, About watches is the company to call.
-          We provide FREE estimates if you live in the local area. Call us today
-          to learn more about our senior citizen and military personnel
-          discounts.
+          <p>
+            With over 27 years of experience, we have been providing exceptional
+            watches services for commercial and residential properties
+            throughout Bellingham. If you're looking for any type of plumbing
+            repairs, installations or replacements, About watches is the company
+            to call. We provide FREE estimates if you live in the local area.
+            Call us today to learn more about our senior citizen and military
+            personnel discounts.
+          </p>
 
-        </p>
-
-        <div className="inputFlex">
-          <div className="inputFlexSub">
-            
-            <div className="textAndInputFlex">
-            <div className="eachInput">
-              <input className='input' type="checkbox" checked />
-            </div>
-            <h1>Best Watch Quality </h1>
-            </div>
-            <div className="textAndInputFlex">
-            <div className="eachInput">
-              <input className='input' type="checkbox" checked />
-            </div>
-            <h1>Watches Service </h1>
-            </div>
-            <div className="textAndInputFlex">
-            <div className="eachInput">
-              <input className='input' type="checkbox" checked />
-            </div>
-            <h1>Free internet Service  </h1>
+          <div className="inputFlex">
+            <div className="inputFlexSub">
+              <div className="textAndInputFlex">
+                <div className="eachInput">
+                  <input className="input" type="checkbox" checked />
+                </div>
+                <h1>Best Watch Quality </h1>
+              </div>
+              <div className="textAndInputFlex">
+                <div className="eachInput">
+                  <input className="input" type="checkbox" checked />
+                </div>
+                <h1>Watches Service </h1>
+              </div>
+              <div className="textAndInputFlex">
+                <div className="eachInput">
+                  <input className="input" type="checkbox" checked />
+                </div>
+                <h1>Free internet Service </h1>
+              </div>
             </div>
 
+            <div className="inputFlexSub">
+              <div className="textAndInputFlex">
+                <div className="eachInput">
+                  <input className="input" type="checkbox" checked />
+                </div>
+                <h1>Modern Accessories </h1>
+              </div>
+              <div className="textAndInputFlex">
+                <div className="eachInput">
+                  <input className="input" type="checkbox" checked />
+                </div>
+                <h1>Watches in Colors </h1>
+              </div>
+              <div className="textAndInputFlex">
+                <div className="eachInput">
+                  <input className="input" type="checkbox" checked />
+                </div>
+                <h1>Free app installation </h1>
+              </div>
+            </div>
           </div>
-
-          <div className="inputFlexSub">
-            
-            <div className="textAndInputFlex">
-            <div className="eachInput">
-              <input className='input' type="checkbox" checked />
-            </div>
-            <h1>Modern Accessories </h1>
-            </div>
-            <div className="textAndInputFlex">
-            <div className="eachInput">
-              <input className='input' type="checkbox" checked />
-            </div>
-            <h1>Watches in Colors </h1>
-            </div>
-            <div className="textAndInputFlex">
-            <div className="eachInput">
-              <input className='input' type="checkbox" checked />
-            </div>
-            <h1>Free app installation </h1>
-            </div>
-
-          </div>
-
         </div>
-       </div>
 
-       <div className="bestWatchCarrier">
+        <div className="bestWatchCarrier">
+          <img className="best-Watch" src={watch} alt="best-watch" />
 
-  <img className="best-Watch" src={watch} alt="best-watch" />
+          <div className="seeAll">
+            <h3>See all the New Watches </h3>
 
-<div className="seeAll">
-<h3>See all the New Watches </h3>
-
-<p>professional affordable and we always leave the seat down </p>
-</div>
-    
-
-       </div>
+            <p>professional affordable and we always leave the seat down </p>
+          </div>
+        </div>
       </section>
-
-
 
       <section className="aboutWrap reverse">
-       
+        <div className="bestWatchCarrier">
+          <img className="best-Watch" src={watch2} alt="best-watch" />
+        </div>
+        <div className="aboutWrapSub">
+          <h1>
+            We are expert in <span>Watches</span>
+          </h1>
 
-       <div className="bestWatchCarrier">
+          <p>
+            With over 27 years of experience, we have been providing exceptional
+            watches services for commercial and residential properties
+            throughout Bellingham. If you're looking for any type of plumbing
+            repairs, installations or replacements, About watches is the company
+            to call.
+          </p>
+          <div className="ratingWrap2">
+            <div>
+              <div className="flexRate">
+                <h3>Watches Quality </h3>
+                <div>
+                  <button>99%</button>
+                </div>
+              </div>
+              <div className="lineCarrier">
+                <div className="width1 width"></div>
+              </div>
+            </div>
 
-  <img className="best-Watch" src={watch2} alt="best-watch" />
-
-
-    
-
-       </div>
-       <div className="aboutWrapSub">
-       <h1>
-          We are expert in <span>Watches</span> 
-        </h1>
-
-        <p>
-          With over 27 years of experience, we have been providing exceptional
-          watches services for commercial and residential properties throughout
-          Bellingham. If you're looking for any type of plumbing repairs,
-          installations or replacements, About watches is the company to call.
-       
-        </p>
-        <div className="ratingWrap2">
-
-         <div>
-         <div className="flexRate">
-            <h3>Watches Quality </h3>
-            <div >
-              <button>99%</button>
+            <div>
+              <div className="flexRate">
+                <h3>Guarantee and Support </h3>
+                <div>
+                  <button>98%</button>
+                </div>
+              </div>
+              <div className="lineCarrier">
+                <div className="width2 width"></div>
+              </div>
+            </div>
+            <div>
+              <div className="flexRate">
+                <h3>Other watches services </h3>
+                <div>
+                  <button>65%</button>
+                </div>
+              </div>
+              <div className="lineCarrier">
+                <div className="width3 width"></div>
+              </div>
             </div>
           </div>
-          <div className="lineCarrier">
-            <div className="width1 width"></div>
-          </div>
-         </div>
+        </div>
+      </section>
 
-         <div>
-         <div className="flexRate">
-            <h3>Guarantee and Support  </h3>
-            <div >
-              <button>98%</button>
+      <section className="factWrap">
+        <ScrollTrigger
+          className=""
+          onEnter={() => setCounterOn(true)}
+          onExit={() => setCounterOn(false)}>
+          <div className="fact">
+            <div className="line">
+              <h2>our facts and figures </h2>
             </div>
           </div>
-          <div className="lineCarrier">
-            <div className="width2 width"></div>
-          </div>
-         </div>
-         <div>
-         <div className="flexRate">
-            <h3>Other watches services  </h3>
-            <div >
-              <button>65%</button>
+          <div className="figureBg">
+            <p>
+              You can trust our watches and quality and professionalism of our
+              products{" "}
+            </p>
+
+            <div className="numbersFlex">
+              <div>
+                <h1>
+                  {" "}
+                  <CountUp
+                    className="text-2xl md:text-5xl"
+                    start={0}
+                    end={27}
+                    duration={2}
+                    delay={0}
+                  />{" "}
+                </h1>
+
+                <button className="px-4 font-bold">Years of experience </button>
+              </div>
+
+              <div>
+                <h1>
+                  {" "}
+                  <CountUp
+                    className="text-2xl md:text-5xl"
+                    start={0}
+                    end={34}
+                    duration={2}
+                    delay={0}
+                  />{" "}
+                </h1>
+
+                <button className="px-4 font-bold">Expert Supports</button>
+              </div>
+
+              <div>
+                <h1>
+                  {" "}
+                  <CountUp
+                    className="text-2xl md:text-5xl"
+                    start={0}
+                    end={16}
+                    duration={2}
+                    delay={0}
+                  />{" "}
+                </h1>
+
+                <button className="px-4 font-bold">Awards Winners </button>
+              </div>
+
+              <div>
+                <h1>
+                  {" "}
+                  <CountUp
+                    className="text-2xl md:text-5xl"
+                    start={0}
+                    end={800}
+                    duration={2}
+                    delay={0}
+                  />{" "}
+                </h1>
+
+                <button className="px-4 font-bold">Happy Clients </button>
+              </div>
             </div>
           </div>
-          <div className="lineCarrier">
-            <div className="width3 width"></div>
-          </div>
-         </div>
+        </ScrollTrigger>
+      </section>
+
+      <section className="joinWrap">
+        <div className="join">
+          <h1>
+            Join our <span>happy customers</span>{" "}
+          </h1>
+          <p>
+            You can trust our watches and the quality and professionalism of our
+            products.
+          </p>
         </div>
 
-   
-       </div>
+        <Slider {...settings} className="slider-carrier py-8">
+          <div className="slider">
+            <h1>This Watch is amazing affordable price </h1>
+
+            <p>
+              {" "}
+              I don't always clop, but when i do, it's because of pear watch.
+              pear watch has really helped me. i STRONGLY recommend pear watch
+              to EVERYONE interested in fashion&tech This is simply unbelievable{" "}
+            </p>
+            <div className="imgAndText2 py-6">
+              <div>
+                <img src={testimonial1} alt="testimonial-img" />
+              </div>
+
+              <div>
+                <h5>John Carter</h5>
+                <p>UX Designer </p>
+              </div>
+            </div>
+          </div>
+          <div className="slider">
+            <h1>This Watch is amazing affordable price </h1>
+
+            <p>
+              {" "}
+              I don't always clop, but when i do, it's because of pear watch.
+              pear watch has really helped me. i STRONGLY recommend pear watch
+              to EVERYONE interested in fashion&tech This is simply unbelievable{" "}
+            </p>
+
+            <div className="imgAndText2 py-6">
+              <div>
+                <img src={testimonial2} alt="testimonial-img" />
+              </div>
+
+              <div>
+                <h5 className="change2">Angela kidjo</h5>
+                <p>UX Designer </p>
+              </div>
+            </div>
+          </div>
+        </Slider>
       </section>
-  
-  <section className="factWrap">
-
-  <div className="fact">
-    
-    <div className="line">
-    <h2>our facts and figures </h2>
-    </div>
-
-  </div>
-  <div className="figureBg">
-    <p>You can trust our watches and quality and professionalism of our products  </p>
-
-    <div className="numbersFlex">
-    <div>
-      <h1>27</h1>
-
-      <button>Years of experience </button>
-    </div>
-
-    <div>
-      <h1>34</h1>
-
-      <button>Expert Supports  </button>
-    </div>
-    <div>
-      <h1>16</h1>
-
-      <button>Awards Winners  </button>
-    </div>
-    <div>
-      <h1>800</h1>
-
-      <button>Happy Clients </button>
-    </div>
-
-    </div>
-
-  </div>
-
-  </section>
-
-  <section className="joinWrap">
-    
-<div className="join">
-  <h1>Join our <span>happy customers</span> </h1>
-  <p>You can trust our watches and the quality and professionalism of our products.</p>
-
-</div>
-
-<Slider {...settings} className="slider-carrier">
-
-
-<div className="slider">
-  <h1>This Watch is amazing affordable price </h1>
-
-  <p> I don't always clop, but when i do, it's because of pear watch. pear
-            watch has really helped me. i STRONGLY recommend pear watch to
-            EVERYONE interested in fashion&tech This is simply unbelievable{" "}</p>
-            <div className="imgAndText2">
-            <div>
-              <img src={testimonial1} alt="testimonial-img" />
-            </div>
-
-            <div>
-              <h5 >John Carter</h5>
-              <p>UX Designer </p>
-            </div>
-          </div>
-</div>
-<div className="slider">
-  <h1>This Watch is amazing affordable price </h1>
-
-  <p> I don't always clop, but when i do, it's because of pear watch. pear
-            watch has really helped me. i STRONGLY recommend pear watch to
-            EVERYONE interested in fashion&tech This is simply unbelievable{" "}</p>
-
-            <div className="imgAndText2">
-            <div>
-              <img src={testimonial2} alt="testimonial-img" />
-            </div>
-
-            <div>
-              <h5 className="change2"  >Angela kidjo</h5>
-              <p>UX Designer </p>
-            </div>
-          </div>
-</div>
-
-</Slider>
-
-
-
-
-  </section >
     </div>
   );
 };
